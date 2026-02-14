8b57464fbd86664deaf4b8533982d2f6facc2880
Hai Bui <buihahaivas@gmail.com>
2026-02-14 18:06:20 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-[32px] font-[700]">Trang chủ</h1>
+      </div>
+    </>
+  );
+}
