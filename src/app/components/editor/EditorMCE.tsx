a644801fd3aff4a6a16a9f0914e270fa8b41c42d
Hai Bui <buihahaivas@gmail.com>
2026-03-01 19:40:41 +0700

    Tích hợp bộ soạn thảo TinyMCE - FE

---
+++ src/app/components/editor/EditorMCE.tsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+import { Editor } from '@tinymce/tinymce-react';
+
+export const EditorMCE = (props: {
+  editorRef: any,
+  value?: string
+}) => {
+  const { editorRef, value="" } = props;
+
+  return (
+    <>
+      <Editor 
+        apiKey={process.env.NEXT_PUBLIC_API_TINYMCE_KEY}
+        onInit={ (_evt, editor) => editorRef.current = editor }
+        initialValue={value}
+        init={{
+          height: 500,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount',
+          ],
+          toolbar: 'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          images_upload_url: `${process.env.NEXT_PUBLIC_API_URL}/upload/image`
+        }}
+      />
+    </>
+  )
+}
