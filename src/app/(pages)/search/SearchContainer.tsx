3b1f1aa73094dab1c28216ecffef53638d582d0d
Hai Bui <buihahaivas@gmail.com>
2026-03-15 10:58:56 +0700

    Bộ lọc theo hình thức làm việc - FE

--- src/app/(pages)/search/SearchContainer.tsx
+++ src/app/(pages)/search/SearchContainer.tsx
@@ -11,18 +11,19 @@ export const SearchContainer = () => {
   const company = searchParams.get("company") || "";
   const keyword = searchParams.get("keyword") || "";
   const position = searchParams.get("position") || "";
+  const workingFrom = searchParams.get("workingFrom") || "";
   const [jobList, setJobList] = useState<any[]>([]);
   
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/search?language=${language}&city=${city}
-    &company=${company}&keyword=${keyword}&position=${position}`)
+    &company=${company}&keyword=${keyword}&position=${position}&workingFrom=${workingFrom}`)
       .then(res => res.json())
       .then(data => {
         if(data.code == "success") {
           setJobList(data.jobs);
         }
       })
-  }, [language, city, company]);
+  }, [language, city, company, keyword, position, workingFrom]);
 
   const handleFilterPosition = (event: any) => {
     const position = event.target.value;
@@ -35,12 +36,23 @@ export const SearchContainer = () => {
     router.push(`?${params.toString()}`);
   }
 
+  const handleFilterWorkingFrom = (event: any) => {
+    const workingFrom = event.target.value;
+    const params = new URLSearchParams(searchParams.toString());
+    if(workingFrom) {
+      params.set("workingFrom", workingFrom);
+    } else {
+      params.delete("workingFrom");
+    }
+    router.push(`?${params.toString()}`);
+  }
+
   return (
     <>
       <div className="py-[60px]">
         <div className="container mx-auto px-[16px]">
           <h2 className="font-[700] text-[28px] text-[#121212] mb-[30px]">
-            {jobList.length} việc làm <span className="text-[#0088FF]">{language} {city} {company} {keyword} {position}</span>
+            {jobList.length} việc làm <span className="text-[#0088FF]">{language} {city} {company} {keyword} {position} {workingFrom}</span>
           </h2>
           <div 
             className="bg-white rounded-[8px] py-[10px] px-[20px] mb-[30px] flex flex-wrap gap-[12px]"
@@ -62,11 +74,16 @@ export const SearchContainer = () => {
               <option value="Senior">Senior</option>
               <option value="Manager">Manager</option>
             </select>
-            <select name="" className="border-[1px] border-[#DEDEDE] rounded-[20px] h-[36px] px-[18px] font-[400] text-[16px] text-[#414042]">
+            <select 
+              name="" 
+              className="border-[1px] border-[#DEDEDE] rounded-[20px] h-[36px] px-[18px] font-[400] text-[16px] text-[#414042]"
+              onChange={handleFilterWorkingFrom}
+              defaultValue={workingFrom}
+            >
               <option value="">Hình thức làm việc</option>
-              <option value="">Tại văn phòng</option>
-              <option value="">Làm từ xa</option>
-              <option value="">Linh hoạt</option>
+              <option value="office">Tại văn phòng</option>
+              <option value="remote">Làm từ xa</option>
+              <option value="flexible">Linh hoạt</option>
             </select>
           </div>
           <div className="mb-[30px] grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[20px]">
